e14aab629f8aa11e1f8a3e80a41726f77a3ced95
venky <gamesf2pmode@gmail.com>
2025-10-13 16:22:28 -0400

    Completed web app deploy script

--- script.sh
+++ script.sh
@@ -1,3 +1,20 @@
+
 #!/bin/bash
 echo "Deploying Webapps on NGINX"
 
+echo "Updating System"
+sudo apt update -y
+
+echo "Install Utilities"
+sudo apt install -y zip unzip
+
+echo "Install NGINX"
+sudo apt install -y nginx
+
+echo "Remove Old Files from /var/www/html"
+sudo rm -r /var/www/html
+
+echo "Deploy Login App"
+sudo git clone https://github.com/gamesf2pmode@gmail.com/webapp.git
+
+echo "Deployed Webapps On NGINX" 
